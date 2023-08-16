707937d0607990c0ad22a3849b5a3ecc543afc34
victoroab <96608980+victoroab@users.noreply.github.com>
2023-08-16 14:57:24 +0100

    set up template for building cli

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,53 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {}
+import { Command } from 'commander'
 
-console.log('WOW')
+const program = new Command()
+
+program
+  .argument('<string>', 'string to log')
+  .action((message: string) => {
+    console.log(`Hello ${message}`)
+  })
+  .description('Say Hello')
+
+program
+  .command('add <numbers...>')
+  .action((numbers: string[]) => {
+    // const total = numbers.reduce((a, b) => a + b, 0)
+    // console.log(`Total: ${total}`)
+    let inc = 0
+    for (let i = 0; i < numbers.length; i++) {
+      const conv = parseInt(numbers[i]!)
+      inc += conv
+    }
+    console.log(inc.toLocaleString())
+  })
+  .description('Add numbers and log the total')
+
+program
+  .command('get-max-number <numbers...>')
+  .action((numbers: string[]) => {
+    // let numbers2 = [...numbers]
+    // for (let i = 0; i < numbers2.length; i++) {
+    //   for (let j = 0; j < numbers2.length - 1 - i; j++) {
+    //     if (numbers2[j] > numbers2[j + 1]) {
+    //       const tmp = numbers2[j]
+    //       numbers2[j] = numbers2[j + 1]
+    //       numbers2[j + 1] = tmp
+    //     }
+    //   }
+    // }
+    // console.log(numbers2[numbers2.length - 1])
+    const nums: number[] = numbers.map((s: string) => parseInt(s))
+    let max = 0
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i]! > max) {
+        max = nums[i]!
+      }
+    }
+    console.log(max)
+  })
+  .description('Get the greatest number out of a set of numbers')
+
+program.parse(process.argv)
